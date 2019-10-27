6bfb96742389b6124520f20bc4e7259facd22139
Demi91 <lanluo_bingzi@126.com>
2019-10-27 20:29:17 +0800

    spring-boot-06-logging

---
+++ spring-boot-duing-06-logging/src/main/java/com/duing/SpringBootDuing06LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.duing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDuing06LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDuing06LoggingApplication.class, args);
+    }
+
+}
